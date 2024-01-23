c59dee5d175a5aae765c65376a6b46e79fc53ab9
John Schmidt <john@schmidthaus.rocks>
2024-01-23 12:37:22 -0800

    Reverting 2a1a707 Refactored to use REACT hooks.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,44 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import CardList from '../components/CardList';
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
-export default function App() {
-  const [robots, setRobots] = useState([]);
-  const [searchfield, setSearchfield] = useState('');
-
-  useEffect(() => {
-    fetch('https:jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => setRobots(users));
-  },[]);
-
-  const onSearchChange = (event) => {
-    setSearchfield(event.target.value);
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: [],
+      searchfield: '',
+    }
   }
 
-  const filteredRobots = robots.filter(robot => {
-    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-  });
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  }
 
-  return !robots.length ?
-    (
-      <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
-        <h1 className='f2'>Loading</h1>
-      </div>
-    ) :
-    (
-      <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={onSearchChange} />
-        <Scroll>
-          <ErrorBoundary fallback={<h2>Oooops. That is not good.</h2>}>
-            <CardList robots={filteredRobots} />
-          </ErrorBoundary>
-        </Scroll>
-      </div>
-    );
+  render() {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    });
+    return !robots.length ?
+      (
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <h1 className='f2'>Loading</h1>
+        </div>
+      ) :
+      (
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <ErrorBoundary fallback={<h2>Oooops. That is not good.</h2>}>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundary>
+          </Scroll>
+        </div>
+      );
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
+  }
 }
+
+export default App;
