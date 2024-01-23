28d0fefa1210e2c1bb5bbd62775cd7e317235e7c
John Schmidt <john@schmidthaus.rocks>
2024-01-23 14:56:25 -0800

    Fixed last commit.

--- src/containers/App.js
+++ src/containers/App.js
@@ -14,11 +14,16 @@ class App extends Component {
     }
   }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
+  }
+
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value });
   }
 
-<<<<<<< Updated upstream
   render() {
     const { robots, searchfield } = this.state;
     const filteredRobots = robots.filter(robot => {
@@ -45,32 +50,6 @@ class App extends Component {
         </div>
       );
   }
-
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ robots: users }));
-  }
-=======
-  return !robots.length ?
-    (
-      <div className='tc'>
-        <h1 className='f1 fw2'>RoboFriends</h1>
-        <h1 className='f2'>Loading</h1>
-      </div>
-    ) :
-    (
-      <div className='tc'>
-        <h1 className='f1 fw2'>RoboFriends</h1>
-        <SearchBox searchChange={onSearchChange} />
-        <Scroll>
-          <ErrorBoundary fallback={<h2>Oooops. That is not good.</h2>}>
-            <CardList robots={filteredRobots} />
-          </ErrorBoundary>
-        </Scroll>
-      </div>
-    );
->>>>>>> Stashed changes
 }
 
 export default App;
