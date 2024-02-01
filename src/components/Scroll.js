c85e033d214f3b993c15dc324643ee3aeb33044b
John Schmidt <john@schmidthaus.rocks>
2024-01-31 20:09:21 -0800

    Hide scroll bar.

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Scroll.css';
 
 const Scroll = (props) => {
   return(
-    <div style={{ overflowY: 'scroll', flex: '0 1 auto' }}>
+    <div className='hideScrollBar' style={{ overflowY: 'scroll', flex: '0 1 auto' }}>
       {props.children}
     </div>
   );
