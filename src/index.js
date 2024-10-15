0dbb4759a163aa61d2653290c5a0b688bbb1952f
John Schmidt <john@schmidthaus.rocks>
2024-10-15 11:42:39 -0700

    Initial addition of a service worker for a PWA
    Signed-off-by: John Schmidt <john@schmidthaus.rocks>

--- src/index.js
+++ src/index.js
@@ -7,16 +7,18 @@ import {
   compose,
   combineReducers,
 } from 'redux';
-import { createLogger } from 'redux-logger';
+// import { createLogger } from 'redux-logger';
 import { thunk } from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import { searchRobots, requestRobots } from './reducers';
 import 'tachyons';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+// Need to find a way to only enable this in dev, same as logger.
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const logger = createLogger();
+// const logger = createLogger();
 
 const rootReducer = combineReducers({ searchRobots, requestRobots });
 
@@ -34,4 +36,12 @@ root.render(
   </React.StrictMode>,
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
