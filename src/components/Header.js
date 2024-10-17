2d34ed39d5c87b259206632afd00601d677b81ff
John Schmidt <john@schmidthaus.rocks>
2024-10-17 17:38:07 +0000

    Added CounterButton
    Signed-off-by: John Schmidt <john@schmidthaus.rocks>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,22 @@
 import { Component } from 'react';
+import CounterButton from './CounterButton';
+
+// In most cases, instead of writing shouldComponentUpdate()
+// by hand, you can inherit from React.PureComponent. It is
+// equivalent to implementing shouldComponentUpdate() with a
+// shallow comparison of current and previous props and state.
 
 class Header extends Component {
   shouldComponentUpdate(nextProps, nextState) {
     return false; // this is a dumb component so we can do this.
   }
   render() {
-    return <h1 className='f1-ns fw2'>RoboFriends</h1>;
+    return (
+      <>
+        <h1 className='f1-ns fw2'>RoboFriends</h1>
+        <CounterButton />
+      </>
+    );
   }
 }
 
