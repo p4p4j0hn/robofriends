647c1aa268c28fc34c20e51193093f32c98f2fbd
John Schmidt <john@schmidthaus.rocks>
2024-10-18 23:46:22 +0000

    Added apiCall function. Formatting.
    Signed-off-by: John Schmidt <john@schmidthaus.rocks>

--- src/actions.js
+++ src/actions.js
@@ -2,20 +2,19 @@ import {
   CHANGE_SEARCHFIELD,
   REQUEST_ROBOTS_PENDING,
   REQUEST_ROBOTS_SUCCESS,
-  REQUEST_ROBOTS_FAILED,
+  REQUEST_ROBOTS_FAILED
 } from './constants.js';
+
+import { apiCall } from './api/api.js';
 
 export const setSearchField = (text) => ({
   type: CHANGE_SEARCHFIELD,
-  payload: text,
+  payload: text
 });
 
 export const requestRobots = () => (dispatch) => {
   dispatch({ type: REQUEST_ROBOTS_PENDING });
-  fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response) => response.json())
+  apiCall('https://jsonplaceholder.typicode.com/users')
     .then((data) => dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data }))
-    .catch((error) =>
-      dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error })
-    );
+    .catch((error) => dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error }));
 };
