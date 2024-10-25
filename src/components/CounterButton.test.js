82e0655098347e646e195b7d3bdde0563e1b1a63
John Schmidt <john@schmidthaus.rocks>
2024-10-24 22:25:52 -0700

    Updated to use @testing-library

--- src/components/CounterButton.test.js
+++ src/components/CounterButton.test.js
@@ -1,31 +1,36 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+import { fireEvent, render, screen } from '@testing-library/react';
 import CounterButton from './CounterButton';
 
 describe('counterbutton', () => {
-  it('expect to render CounterButton component', () => {
+  test('expect to render CounterButton component without crashing', () => {
     expect.assertions(1);
     const mockColour = 'red';
-    expect(shallow(<CounterButton color={mockColour} />)).toMatchSnapshot();
+    expect(renderer.create(<CounterButton color={mockColour} />).toJSON()).toMatchSnapshot();
   });
 
-  it('correctly increments the counter', () => {
-    expect.assertions(3);
-    const mockColour = 'red';
-    const wrapper = shallow(<CounterButton color={mockColour} />);
-    expect(wrapper).toMatchSnapshot();
-    wrapper.find('[id="counter"]').simulate('click');
-    expect(wrapper.state()).toStrictEqual({ count: 1 });
-    wrapper.find('[id="counter"]').simulate('click');
-    wrapper.find('[id="counter"]').simulate('click');
-    expect(wrapper.state()).toStrictEqual({ count: 3 });
-  });
-
-  it('only re-renders when required', () => {
+  test('correctly increments the counter', async () => {
     expect.assertions(2);
     const mockColour = 'red';
-    const wrapper = shallow(<CounterButton color={mockColour} />);
-    expect(wrapper.instance().shouldComponentUpdate({ count: 0 })).toBe(false);
-    expect(wrapper.instance().shouldComponentUpdate({ count: 4 })).toBe(true);
+    render(<CounterButton color={mockColour} />);
+    const element = screen.getByText(/count/i);
+    await fireEvent.click(element);
+    expect(element).toHaveTextContent('Count: 1');
+    await fireEvent.click(element);
+    await fireEvent.click(element);
+    expect(element).toHaveTextContent('Count: 3');
   });
+
+  // React Testing Library recommends only testing what the user sees,
+  // not the implementation. So I'm not sure this test is required
+  // even though the coverage won't be 100%
+  // test('only re-renders when required', async () => {
+  //   const mockColour = 'red';
+  //   const wrapper = shallow(<CounterButton color={mockColour} />);
+  //   wrapper.find('[id="counter"]').simulate('click');
+  //   wrapper.find('[id="counter"]').simulate('click');
+  //   wrapper.find('[id="counter"]').simulate('click');
+  //   expect(wrapper.instance().shouldComponentUpdate({ count: 0 })).toBe(false);
+  //   expect(wrapper.instance().shouldComponentUpdate({ count: 4 })).toBe(true);
+  // });
 });
