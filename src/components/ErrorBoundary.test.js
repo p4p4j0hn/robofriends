40506809b6cb4d06b42012d045554b6cb8702c48
John Schmidt <john@schmidthaus.rocks>
2024-10-31 10:46:26 -0700

    Updated to use @testing-library

--- src/components/ErrorBoundary.test.js
+++ src/components/ErrorBoundary.test.js
@@ -1,28 +1,57 @@
-import React from 'react';
-import { shallow, mount } from 'enzyme';
+import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
 import ErrorBoundary from './ErrorBoundary';
 
 describe('errorboundary', () => {
   it('expect to render ErrorBoundary component', () => {
     expect.assertions(1);
-    expect(shallow(<ErrorBoundary />)).toMatchSnapshot();
+    expect(
+      renderer
+        .create(
+          <ErrorBoundary fallback={<div>sorry</div>}>
+            <p>Everything is fine</p>
+          </ErrorBoundary>
+        )
+        .toJSON()
+    ).toMatchSnapshot();
+  });
+});
+
+describe('ErrorBoundary', () => {
+  // A very buggy component
+  const ThrowError = () => {
+    throw new Error('Test');
+  };
+
+  // Temporarily suppress console errors so we don't clog the logs
+  const realError = console.error;
+  beforeEach(() => {
+    console.error = jest.fn();
+  });
+  afterEach(() => {
+    console.error = realError;
   });
 
-  it('expect the component to catch', () => {
-    expect.assertions(4);
-    const MyComponent = () => <div>Render</div>;
-    const DecoratedComponent = () => {
-      return (
-        <ErrorBoundary fallback={<div>Fallback</div>}>
-          <MyComponent />
-        </ErrorBoundary>
-      );
-    };
-    const wrapper = mount(<DecoratedComponent />);
-    expect(wrapper.contains(<div>Render</div>)).toBe(true);
-    expect(wrapper.contains(<div>Fallback</div>)).toBe(false);
-    wrapper.find(MyComponent).simulateError(new Error('Error While Rendering'));
-    expect(wrapper.contains(<div>Render</div>)).toBe(false);
-    expect(wrapper.contains(<div>Fallback</div>)).toBe(true);
+  it('renders children when everything is fine', async () => {
+    expect.assertions(2);
+    render(
+      <ErrorBoundary fallback={<div>sorry</div>}>
+        <p>Everything is fine</p>
+      </ErrorBoundary>
+    );
+    expect(screen.getByText(/Everything is fine/i)).toBeInTheDocument();
+    expect(screen.queryByText(/sorry/i)).not.toBeInTheDocument();
+  });
+
+  it('shows an apologetic error message when an unhandled exception is thrown', async () => {
+    expect.assertions(2);
+    render(
+      <ErrorBoundary fallback={<div>sorry</div>}>
+        <ThrowError />
+        <p>Everything is fine</p>
+      </ErrorBoundary>
+    );
+    expect(screen.getByText(/sorry/i)).toBeInTheDocument();
+    expect(screen.queryByText(/Everything is fine/i)).not.toBeInTheDocument();
   });
 });
